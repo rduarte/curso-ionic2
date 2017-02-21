dea839789012fce3a894d368adb5b6334ff5951b
Ricardo Duarte <ricardo@ricardoduarte.com>
2017-02-21 10:12:14 -0300

    Final da aula 04, seção 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,32 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
-
+import { MenuTestPage } from '../pages/menu-test/menu-test';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage = HomePage;
+  pages: Array<{component: any, title: string, icon: string}>;
 
   constructor(platform: Platform) {
+
+    this.pages = [
+      { component: HomePage, title: 'Home', icon: 'home' }, // Este item está desaparecido.
+      { component: HomePage, title: 'Home', icon: 'home' },
+      { component: MenuTestPage, title: 'Menu de teste', icon: 'menu' }
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  openPage(page: any): void {
+    this.rootPage = page.component;
   }
 }
---
+++ src/pages/menu-test/menu-test.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-menu-test',
+  templateUrl: 'menu-test.html'
+})
+export class MenuTestPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
