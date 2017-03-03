d9e7820e26312290739390d73100e56b9a13ecc8
Ricardo Duarte <ricardo@ricardoduarte.com>
2017-03-02 22:07:33 -0300

    Correção do bug no menu. Obrigado @plinionaves

--- src/pages/generated-test/generated-test.ts
+++ src/pages/generated-test/generated-test.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { ConnectionService } from '../../providers/connection-service'
 
 @Component({
@@ -8,10 +8,30 @@ import { ConnectionService } from '../../providers/connection-service'
 })
 export class GeneratedTestPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private connectionService: ConnectionService) {}
+  cep: string = '?';
 
-  buscarCep() : void{
-    this.connectionService.getCep('88704290')
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, private connectionService: ConnectionService) {}
+
+  pedirCep(): void{
+    let alert = this.alertCtrl.create({
+      'title': 'CEP',
+      'message': 'Digite o seu CEP',
+      'inputs': [
+        { name: 'cep', placeholder: 'Seu CEP'}
+      ],
+      'buttons': [
+        {
+          text: "Ok", handler: (data) => {
+            this.buscarCep(data.cep);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  buscarCep(cep: string): void{
+    this.connectionService.getCep(cep+'')
       .then((res) => {
         let json = res.json();
         console.log(json.localidade);
--- src/pages/menu-test/menu-test.ts
+++ src/pages/menu-test/menu-test.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Lowercase } from '../../pipes/lowercase'
 
 @Component({
   selector: 'page-menu-test',
